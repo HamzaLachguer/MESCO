9fd68e4c2582afc61c53928b8063cbd83501e0cc
hamzalachguer <lachguerhmza@gmail.com>
2025-09-07 01:18:05 +0200

    Country selector

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,13 @@ const headerElements = {
     closeNavMenuBtn: document.querySelector("#close-menu-btn"),
     navMenuContainer: document.querySelector("#navigation-menu"),
     navigationBar: document.querySelector("#main-navigation-bar"),
+  },
+
+  countrySelector: {
+    showCountrySelector: document.querySelector("#country-selector"),
+    showCountrySelectorMobile: document.querySelector("#country-selector-mobile"),
+    countrySelectorContainer: document.querySelector("#country-selector-container"),
+    closeCountrySelector: document.querySelector("#close-country-selector"),
   }
 }
 
@@ -43,7 +50,8 @@ function hidePromoModal() {
   headerElements.promoModal.promoModalContainer.classList.replace("opacity-1", "opacity-0");
   setTimeout(() => {
     headerElements.promoModal.promoModalContainer.classList.replace("grid", "hidden");
-  }, 300)
+  }, 300);
+  document.body.classList.remove("overflow-hidden");
   
   // update aria attributes
   headerElements.promoModal.promoModalContainer.setAttribute("aria-hidden", true);
@@ -61,6 +69,52 @@ function initPromoModal() {
     hidePromoModal();
   });
   headerElements.promoModal.hidePromoModal.addEventListener('click', hidePromoModal);
+}
+
+
+
+
+// function => show promo modal
+function showCountrySelector() {
+
+  headerElements.countrySelector.countrySelectorContainer.classList.replace("hidden", "grid");
+  headerElements.countrySelector.countrySelectorContainer.classList.replace("opacity-0", "opacity-1");
+  document.body.classList.add("overflow-hidden");
+
+  // update aria attributes
+  headerElements.countrySelector.countrySelectorContainer.setAttribute("aria-hidden", false);
+  headerElements.countrySelector.showCountrySelector.setAttribute("aria-expanded", true);
+  headerElements.countrySelector.closeCountrySelector.setAttribute("aria-expanded", true);
+}
+
+// function => hide promo modal
+function hideCountrySelector() {
+  
+  headerElements.countrySelector.countrySelectorContainer.classList.replace("opacity-1", "opacity-0");
+  setTimeout(() => {
+    headerElements.countrySelector.countrySelectorContainer.classList.replace("grid", "hidden");
+  }, 300);
+  document.body.classList.remove("overflow-hidden");
+  
+  // update aria attributes
+  headerElements.countrySelector.countrySelectorContainer.setAttribute("aria-hidden", true);
+  headerElements.countrySelector.showCountrySelector.setAttribute("aria-expanded", false);
+  headerElements.countrySelector.closeCountrySelector.setAttribute("aria-expanded", false);
+}
+
+
+//  promo modal event listeners
+function initCountrySelector() {
+  headerElements.countrySelector.showCountrySelector.addEventListener('click', showCountrySelector);
+
+  headerElements.countrySelector.showCountrySelectorMobile.addEventListener('click', showCountrySelector);
+
+  headerElements.countrySelector.countrySelectorContainer.addEventListener('click', (e) => {
+    if (e.target.closest("#promo-modal-content")) return;
+  
+    hideCountrySelector();
+  });
+  headerElements.countrySelector.closeCountrySelector.addEventListener('click', hideCountrySelector);
 }
 
 
@@ -105,4 +159,5 @@ function initNavigation() {
 document.addEventListener("DOMContentLoaded", () => {
   initPromoModal();
   initNavigation();
+  initCountrySelector();
 })
